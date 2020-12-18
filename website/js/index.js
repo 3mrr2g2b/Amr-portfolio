5ffd4ed8d8775eba0f2200ab9c9e0cbaff1618b7
3mrr2g2b <amrragab28090@gmail.com>
2020-12-18 12:47:26 +0200

    codehub

---
+++ website/js/index.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+    
+    //toggle click 
+    $('.toggle').click(function(){
+			$('.toggle').toggleClass('active');
+			$('.back').toggleClass('active');
+			
+			$('ul').slideToggle(300);
+			
+		});
+	$(".ch_1").click(function(){
+        
+        $('html,body').animate({scrollTop:0},1000);
+      
+    });
+	$(".ch_2").click(function(){
+        
+        $('html,body').animate({scrollTop:1100},1000);
+      
+    });
+	$(".ch_3").click(function(){
+        
+        $('html,body').animate({scrollTop:4600},1000);
+      
+    });
+	$(".button_1").click(function(){
+        
+        $('html,body').animate({scrollTop:4600},1000);
+      
+    });
+		
+		//scroll window
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10 ) {
+        $(".start-begin").addClass("active");
+        
+    } 
+    else {
+        $(".start-begin").removeClass("active");
+    
+    }
+    
+    
+});
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100 ) {
+        
+        $(".sec_3 .box:nth-child(1)").addClass("active");
+    } 
+    else {
+        
+        $(".sec_3 .box:nth-child(1)").removeClass("active");
+    }
+    
+    
+});
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 400 ) {
+        
+        $(".sec_3 .box:nth-child(2)").addClass("active");
+    } 
+    else {
+        
+        $(".sec_3 .box:nth-child(2)").removeClass("active");
+    }
+    
+    
+});
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 800 ) {
+        
+        $(".sec_3 .box:nth-child(3)").addClass("active");
+    } 
+    else {
+        
+        $(".sec_3 .box:nth-child(3)").removeClass("active");
+    }
+    
+    
+});
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >=  4400) {
+        $(".h1_div .h1_connect").addClass("move");
+        $(".connect").addClass("active");
+        $(".connect .container").addClass("active");
+        $(".connect .container .box").addClass("active");
+        
+    } 
+    else {
+        $(".h1_div .h1_connect").removeClass("move");
+        $(".connect").removeClass("active");
+        $(".connect .container").removeClass("active");
+        $(".connect .container .box").removeClass("active");
+        
+    }
+    
+    
+});
+		
+		//type writter code
+		$(".typewritter").typewriter({
+        prefix : "",
+        text : [" I'm Web Designer", "I'm Web Developer", "I'm Grafic Designer","Let's See The Future With Me And Catch Your Dreem. ",'We help you to learn more and more And become Awesome In Many Languages.'],
+        typeDelay : 100,
+        waitingTime : 1500,
+        blinkSpeed : 800
+    });
+});
